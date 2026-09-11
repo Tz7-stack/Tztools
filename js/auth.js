fa9bf318de221633cbb786c467283dbb11d6662c
toezay <toezay77@gmail.com>
2026-09-11 13:01:52 +0100

    Update Supabase publishable key in auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = "https://nslaakklgidpzwlymrhf.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_EXISTING_PUBLISHABLE_KEY_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_XoDQsJkHs_7PA8wQAoutHA_glygnyxK";
 
 const supabaseClient =
   window.supabase &&
